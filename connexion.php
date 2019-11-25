bea890006cb16d6102a20ebc95852d390d6b3f9c
guillaumecolombel1@gmail.com <57393436+Nifooo@users.noreply.github.com>
2019-11-25 11:53:20 +0100

    reparation du footer

--- connexion.php
+++ connexion.php
@@ -2,24 +2,25 @@
 
     <section id="connect">
       <div class="wrap">
-      <p id="con">BIENVENUE</p>
-      <div class="clear"></div>
-      <div class="barre3"></div>
-      <div class="clear"></div>
-      <div class="connexion">
-        <p>Se connecter</p>
-        <form class="" action="accueil.html" method="post">
-          <label for="email"></label>
-          <input type="text" name="email"id="email" placeholder="Email" required>
+          <p id="con">BIENVENUE</p>
           <div class="clear"></div>
-          <label for="password"></label>
-          <input type="password" name="password" id="password" placeholder="Mot de passe" required>
-          <div class="clear">
-            <p>Pas de compte ?<a href="inscription.php"> Inscrivez-vous</a></p>
-
+          <div class="barre3"></div>
+          <div class="clear"></div>
+          <div class="connexion">
+              <p>Se connecter</p>
+            <form class="" action="accueil.html" method="post">
+              <label for="email"></label>
+              <input type="text" name="email"id="email" placeholder="Email" required>
+              <div class="clear"></div>
+              <label for="password"></label>
+              <input type="password" name="password" id="password" placeholder="Mot de passe" required>
+                <p>Pas de compte ?<a href="inscription.php"> Inscrivez-vous</a></p>
+              <input type="submit" name="submit" value="Se connecter">
+            </form>
           </div>
-          <input type="submit" name="submit" value="Se connecter">
-        </form>
+      </div>
+        <div class="clear"></div>
+    </section>
 
 
 <?php include('inc/footer.php'); ?>
