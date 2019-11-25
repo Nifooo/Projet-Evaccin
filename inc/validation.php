6542f0b3257b5f703f33502db2d9a31db81ff55b
Janssen PILLEBOUT <janssen.pillebout@gmail.com>
2019-11-25 11:54:21 +0100

    modification des function

--- inc/validation.php
+++ inc/validation.php
@@ -1,1 +1,14 @@
 <?php
+
+function emailValidation($err, $email, $key)
+{
+    if (!empty($email)){
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $err[$key] = 'Le mail nest pas valide';
+        }
+    } else{
+        $err[$key] = 'Veuillez renseigner ce champ';
+    }
+
+    return $err;
+}
