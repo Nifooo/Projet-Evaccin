564a16c8134ef9ffffd305b45fac1157fee8e637
Janssen PILLEBOUT <janssen.pillebout@gmail.com>
2019-11-29 10:11:45 +0100

    Connexion et inscription et bouton deconnexion fonctionnel !!

--- inc/function.php
+++ inc/function.php
@@ -13,34 +13,3 @@ function debug($tableau) {
     echo '<pre>'; print_r($tableau); echo '</pre>';
 }
 
-///////////////////////////////////////
-// FONCTION DE VALIDATION DE L'EMAIL
-///////////////////////////////////////
-
-function emailValid($err, $mail, $key) {
-    if (!empty($mail)) {
-        if (!filter_var($mail, FILTER_VALIDATE_EMAIL)) {
-            $err[$key] = 'Email non valide';
-        }
-    } else {
-        $err[$key] = "Veuillez renseigner ce champ";
-    }
-    return $err;
-}
-
-///////////////////////////////////////
-// FONCTION DE VALIDATION DES TEXTES
-///////////////////////////////////////
-
-function textValid($err, $text, $key, $x, $y) {
-    if (!empty($text)) {
-        if (mb_strlen($text) < $x) {
-            $err[$key] = 'Minimum '.$x.' caractères';
-        }elseif (mb_strlen($text) > $y) {
-            $err[$key] = 'Maximum '.$y.' caractères';
-        }
-    }else {
-        $err[$key] = 'Veuillez renseigner ce champ';
-    }
-    return $err;
-}
